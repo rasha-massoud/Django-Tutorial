5f02fc92f5e2b5a5795db2147c0b480ae9f17ceb
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-24 00:15:39 +0200

    Updated the index function in the views.py to send data to the template

--- myapp/views.py
+++ myapp/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'name': 'John Doe',
+        'work': 'Developer',
+        'experience': 20,
+    }
+    return render(request, 'index.html', context)
