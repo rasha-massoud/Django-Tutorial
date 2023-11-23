577b995c7827b7afe2e66430176457644c94bb5a
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-24 00:34:30 +0200

    Created the views function for the new path

--- myapp/views.py
+++ myapp/views.py
@@ -10,7 +10,9 @@ def index(request):
     return render(request, 'index.html', context)
 
 def wordCounter(request):
-    context = {
+    return render(request, 'wordCounter.html')
 
-    }
-    return render(request, 'wordCounter.html', context)
+def counter(request):
+    text = request.GET['text']
+    amount_of_words = len(text.split())
+    return render(request, 'counter.html', {'amount': amount_of_words})
