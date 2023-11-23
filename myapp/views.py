b939832eec2c0b908bb65dfecda3e0ec0f0d4a95
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-24 00:55:00 +0200

    Changed the method type to POST

--- myapp/views.py
+++ myapp/views.py
@@ -13,6 +13,6 @@ def wordCounter(request):
     return render(request, 'wordCounter.html')
 
 def counter(request):
-    text = request.GET['text']
+    text = request.POST['text']
     amount_of_words = len(text.split())
     return render(request, 'counter.html', {'amount': amount_of_words})
