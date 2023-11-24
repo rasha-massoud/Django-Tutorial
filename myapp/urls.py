97d930b7001aefc5338379013b8f82013f97420e
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-24 22:06:30 +0200

    Updated the name of the online template url

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.basicIndex, name='basicIndex'),
-    path('online', views.onlineTemplate, name='onlineTemplate'),
+    path('resume', views.onlineTemplate, name='onlineTemplate'),
     path('wordCounter', views.wordCounter, name='wordCounter'),
     path('counter', views.counter, name='counter'),
     path('register', views.register, name='register'),
