f7060141092f76a901a82c347dd9b063bc6cfc93
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-24 19:50:02 +0200

    Created the Feature Model using the model dependency and package

--- myapp/models.py
+++ myapp/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class Feature:
-    id: int
-    name: str
-    details: str
-    svgPath: str
-    iElementClass: str
-    iconColor: str
+class Feature(models.Model):
+    name = models.CharField(max_length=100)
+    details = models.CharField(max_length=500)
+    svgPath = models.CharField(max_length=800)
+    iElementClass = models.CharField(max_length=100)
+    iconColor = models.CharField(max_length=100)
