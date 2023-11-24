9cdc183c4a09bf8c680e1bb70efbcdf2337b8651
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-24 21:45:29 +0200

    Created the path to the login template

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('basic', views.basicIndex, name='basicIndex'),
+    path('', views.basicIndex, name='basicIndex'),
     path('online', views.onlineTemplate, name='onlineTemplate'),
     path('wordCounter', views.wordCounter, name='wordCounter'),
     path('counter', views.counter, name='counter'),
     path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
+
 ]
