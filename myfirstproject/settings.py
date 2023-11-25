d9b65a28acf6548ee97277a0b349c5b951d13455
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-25 16:24:50 +0200

    Updated the databases dictionary in the settings.py for postgresql

--- myfirstproject/settings.py
+++ myfirstproject/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'myfirstproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myfirstproject',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost'
     }
 }
 
